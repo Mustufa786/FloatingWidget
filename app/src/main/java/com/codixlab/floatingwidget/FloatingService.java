d956ce60031c6fd87e97d4d6e632f22d0774187d
Mustufa786 <mustufa_ayub@hotmail.com>
2019-08-04 02:23:17 +0500

    initial commit

---
+++ app/src/main/java/com/codixlab/floatingwidget/FloatingService.java
@@ -0,0 +1,55 @@
+package com.codixlab.floatingwidget;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+public class FloatingService extends Service {
+
+
+    private WindowManager windowManager;
+    private View floatinglayout;
+
+    public FloatingService() {
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        floatinglayout = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT
+        );
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        windowManager.addView(floatinglayout, params);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (floatinglayout != null)
+            windowManager.removeViewImmediate(floatinglayout);
+    }
+}
