62eb6e8c430c965e9f4ea176186e7cbfaaccabbe
Mustufa786 <mustufa_ayub@hotmail.com>
2019-08-04 03:15:08 +0500

    feature done

--- app/src/main/java/com/codixlab/floatingwidget/FloatingService.java
+++ app/src/main/java/com/codixlab/floatingwidget/FloatingService.java
@@ -6,8 +6,11 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FloatingService extends Service {
 
@@ -25,7 +28,7 @@ public class FloatingService extends Service {
 
         floatinglayout = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
@@ -39,6 +42,85 @@ public class FloatingService extends Service {
 
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         windowManager.addView(floatinglayout, params);
+
+
+        final View collapseView = floatinglayout.findViewById(R.id.collapse_layout);
+        final View expandedView = floatinglayout.findViewById(R.id.expanded_layout);
+        ImageButton closeButton = floatinglayout.findViewById(R.id.closeBtn);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                stopSelf();
+            }
+        });
+
+        Button openApp = floatinglayout.findViewById(R.id.openApp);
+        openApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FloatingService.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+                stopSelf();
+            }
+        });
+
+
+        floatinglayout.findViewById(R.id.rootLayout).setOnTouchListener(new View.OnTouchListener() {
+
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+
+                        int xDiff = (int) (event.getRawX() - initialX);
+                        int yDiff = (int) (event.getRawY() - initialY);
+
+                        if(xDiff < 10 && yDiff < 10){
+                            if(isViewCollapsed()){
+                                collapseView.setVisibility(View.GONE);
+                                expandedView.setVisibility(View.VISIBLE);
+
+                            }
+                        }
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+
+                        params.x =  initialX + (int)(event.getRawX()  - initialTouchX);
+                        params.y =  initialY + (int)(event.getRawY()  - initialTouchY);
+
+                        windowManager.updateViewLayout(floatinglayout,params);
+
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private boolean isViewCollapsed() {
+
+        return floatinglayout == null || floatinglayout.findViewById(R.id.collapse_layout).getVisibility() == View.VISIBLE;
     }
 
     @Override
--- app/src/main/java/com/codixlab/floatingwidget/MainActivity.java
+++ app/src/main/java/com/codixlab/floatingwidget/MainActivity.java
@@ -1,13 +1,67 @@
 package com.codixlab.floatingwidget;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final int CODE_DRAW_OVER = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)){
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent,CODE_DRAW_OVER);
+
+        }else{
+            initView();
+        }
+    }
+
+    private void initView() {
+
+
+        Button open= findViewById(R.id.openWidget);
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startService(new Intent(MainActivity.this,FloatingService.class));
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+
+        if(requestCode == CODE_DRAW_OVER){
+            if(resultCode == RESULT_OK){
+                initView();
+            }else{
+                Toast.makeText(this, "Please allow overlay permission", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+
     }
 }
